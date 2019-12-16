89b27468fae0dbacbf227f5855823cc0d0362fe8
Miao Mico <miao.mi.co.io@gmail.com>
2019-12-16 12:46:38 +0800

    chain stack - char 哪里有问题导致 printf 有问题？

--- stack.c
+++ stack.c
@@ -3,17 +3,33 @@
 bool stack_control_init(STACK_TYPEDEF_PTR stack, int depth);
 bool stack_control_free(STACK_TYPEDEF_PTR stack);
 
-bool stack_push(STACK_TYPEDEF_PTR stack, int atom);
-bool stack_pop(STACK_TYPEDEF_PTR stack, int* atom);
-bool stack_multi_pop(STACK_TYPEDEF_PTR stack, int* array, int size);
+bool stack_push(STACK_TYPEDEF_PTR stack, STACK_DATA_TYPE atom);
+bool stack_pop(STACK_TYPEDEF_PTR stack, STACK_DATA_TYPE* atom);
+bool stack_multi_pop(STACK_TYPEDEF_PTR stack, STACK_DATA_TYPE* array, int size);
 
 void stack_default_full_stacl_expection(void);
 void stack_default_null_stacl_expection(void);
+void stack_default_null_heap_expection(void);
+
+bool stack_control_chain_stack_init(CHAIN_STACK_TYPEDEF_PTR stack, int depth);
+bool stack_control_chain_stack_free(CHAIN_STACK_TYPEDEF_PTR stack);
+
+bool chain_stack_push(CHAIN_STACK_TYPEDEF_PTR stack, CHAIN_STACK_DATA_TYPE atom);
+bool chain_stack_pop(CHAIN_STACK_TYPEDEF_PTR stack, CHAIN_STACK_DATA_TYPE* atom);
+bool chain_stack_multi_pop(CHAIN_STACK_TYPEDEF_PTR stack, CHAIN_STACK_DATA_TYPE* array, int size);
+
+void chain_stack_default_full_stacl_expection(void);
+void chain_stack_default_null_stacl_expection(void);
+void chain_stack_default_null_heap_expection(void);
 
 STACK_CONTROL_TYPEDEF stack_ctrl =
 {
     stack_control_init,
-    stack_control_free
+    stack_control_free,
+    {
+        stack_control_chain_stack_init,
+        stack_control_chain_stack_free
+    }
 };
 
 bool stack_control_init(STACK_TYPEDEF_PTR stack,int depth)
@@ -31,6 +47,7 @@ bool stack_control_init(STACK_TYPEDEF_PTR stack,int depth)
 
     stack->expection.full_stack = stack_default_full_stacl_expection;
     stack->expection.null_stack = stack_default_null_stacl_expection;
+    stack->expection.null_heap = stack_default_null_heap_expection;
 
     return true;
 }
@@ -48,7 +65,7 @@ bool stack_control_free(STACK_TYPEDEF_PTR stack)
     return true;
 }
 
-bool stack_push(STACK_TYPEDEF_PTR stack, char atom)
+bool stack_push(STACK_TYPEDEF_PTR stack, STACK_DATA_TYPE atom)
 {
     if(stack->__info.stack_top < stack->__info.stack_depth)    // 如果没有满栈
     {
@@ -64,7 +81,7 @@ bool stack_push(STACK_TYPEDEF_PTR stack, char atom)
     }
 }
 
-bool stack_pop(STACK_TYPEDEF_PTR stack, char* atom)
+bool stack_pop(STACK_TYPEDEF_PTR stack, STACK_DATA_TYPE* atom)
 {
     if(stack->__info.stack_top > 0)                            // 如果没有空栈
     {
@@ -91,7 +108,12 @@ void stack_default_null_stacl_expection(void)
     printf("stack_null_expectopn. \r\n");
 }
 
-bool stack_multi_pop(STACK_TYPEDEF_PTR stack, char* array,int size)
+void stack_default_null_heap_expection(void)
+{
+    printf("heap_null_expectopn. \r\n");
+}
+
+bool stack_multi_pop(STACK_TYPEDEF_PTR stack, STACK_DATA_TYPE* array,int size)
 {
     if (stack->__info.stack_top >= size)                            // 如果没有空栈
     {
@@ -118,6 +140,112 @@ int stack_get_top(STACK_TYPEDEF_PTR stack)
         return false;
 
     return temp;
+}
+
+bool stack_control_chain_stack_init(CHAIN_STACK_TYPEDEF_PTR chain_stack, int max_depth)
+{
+    if ((chain_stack->top = (CHAIN_STACK_NODE_TYPEDEF_PTR)malloc(sizeof(CHAIN_STACK_NODE_TYPEDEF))) == NULL)
+    {
+        chain_stack->expection.null_heap();
+
+        return false;
+    }
+
+    chain_stack->__info.stack_crt_depth = 0;
+    chain_stack->__info.stack_max_depth = max_depth;
+
+    chain_stack->push = chain_stack_push;
+    chain_stack->pop = chain_stack_pop;
+//    chain_stack->multi_pop = chain_stack_multi_pop;
+
+    chain_stack->expection.full_stack = chain_stack_default_full_stacl_expection;
+    chain_stack->expection.null_stack = chain_stack_default_null_stacl_expection;
+    chain_stack->expection.null_heap = chain_stack_default_null_heap_expection;
+
+    return true;
+}
+
+bool stack_control_chain_stack_free(CHAIN_STACK_TYPEDEF_PTR chain_stack)
+{
+    if (chain_stack->__info.stack_malloc == true)
+    {
+        free(chain_stack->top);
+
+        chain_stack->__info.stack_malloc = false;
+        chain_stack->top = NULL;
+    }
+
+    return true;
+}
+
+bool chain_stack_push(CHAIN_STACK_TYPEDEF_PTR chain_stack, CHAIN_STACK_DATA_TYPE atom)
+{
+    CHAIN_STACK_NODE_TYPEDEF_PTR 
+        stack_node_temp = (CHAIN_STACK_NODE_TYPEDEF_PTR)calloc(1,sizeof(CHAIN_STACK_NODE_TYPEDEF));
+
+ //   char* stack_node_data_temp = (char*)calloc(2, sizeof(char));
+
+    if (stack_node_temp == NULL)
+    {
+        chain_stack->expection.null_heap();
+
+        return false;
+    }
+    if(chain_stack->__info.stack_max_depth <= chain_stack->__info.stack_crt_depth)
+    {
+        chain_stack->expection.full_stack();
+
+        return false;
+    }
+
+    stack_node_temp->data = atom;
+    stack_node_temp->next = chain_stack->top->next;
+
+    chain_stack->top->next = stack_node_temp;
+    
+    chain_stack->__info.stack_crt_depth++;
+
+    return true;
+}
+
+bool chain_stack_pop(CHAIN_STACK_TYPEDEF_PTR chain_stack, CHAIN_STACK_DATA_TYPE* atom)
+{
+    CHAIN_STACK_NODE_TYPEDEF_PTR
+        stack_node_temp = NULL;
+
+    if (chain_stack->__info.stack_crt_depth == 0)
+    {
+        chain_stack->expection.null_stack();
+
+        return false;
+    }
+    
+    stack_node_temp = chain_stack->top->next;
+
+    chain_stack->top->next = stack_node_temp->next;
+
+    *atom = stack_node_temp->data;
+
+    chain_stack->__info.stack_crt_depth--;
+
+    free(stack_node_temp);    /* 释放存储空间 */
+
+    return true;
+}
+
+void chain_stack_default_full_stacl_expection(void)
+{
+    printf("stack_full_expectopn. \r\n");
+}
+
+void chain_stack_default_null_stacl_expection(void)
+{
+    printf("stack_null_expectopn. \r\n");
+}
+
+void chain_stack_default_null_heap_expection(void)
+{
+    printf("heap_null_expectopn. \r\n");
 }
 
 float _data_verify_percent(int* input, int* rule,int sizeof_rule)     // 数据正确率
@@ -188,33 +316,55 @@ float data_verify_percent(int* input, int* rule, int sizeof_rule)     // 数据�
 
 void main()
 {
+  //  char atom = 0;
+  //  char array[100] = { 0 };
+  //  STACK_TYPEDEF stack;
+
+  //  stack_ctrl.init(&stack,100);
+
+  //  for (size_t i = 0; i < 10; i++)
+  //  {
+  //      stack.push(&stack, i + '0');
+  //  }
+  //  stack.multi_pop(&stack, array,10);
+
+  //stack_pop(&stack, &atom);
+
+  //printf("pop: %d ", atom);
+
+  //  for (size_t i = 0; i < 10; i++)
+  //  {
+  //      printf("pop: %c ", array[i]);
+  //  }
+
+  //  stack_ctrl.free(&stack);
+
+  //  printf("\r\n");
+
     char atom = 0;
     char array[100] = { 0 };
-    STACK_TYPEDEF stack;
+    CHAIN_STACK_TYPEDEF chain_stack;
 
-    stack_ctrl.init(&stack,100);
-
-    for (size_t i = 0; i < 10; i++)
-    {
-        stack.push(&stack, i + '0');
-    }
-    stack.multi_pop(&stack, array,10);
-
-//  stack_pop(&stack, &atom);
-
-//  printf("pop: %d ", atom);
+    stack_ctrl.chain_stack.init(&chain_stack, 10);
 
     for (size_t i = 0; i < 10; i++)
     {
-        printf("pop: %c ", array[i]);
+        chain_stack.push(&chain_stack, i + '0');
     }
 
-    stack_ctrl.free(&stack);
+    for (size_t i = 0; i < 10; i++)
+    {
+        chain_stack.pop(&chain_stack, &array[i]);
 
-    printf("\r\n");
+     
+    }
+
+    stack_ctrl.free(&chain_stack);
+
+    printf("\r\nend\r\n");
 
     //int
-    //    array[10] = { 1,1,3,0,5,6,7,0,9,1 },
+    //    array[10] = { 0,1,3,0,5,6,7,0,9,1 },
     //    rule[10] = { 1,2,3,4,5,6,7,8,9,1 };
 
     //int size = 10;
--- stack.h
+++ stack.h
@@ -7,17 +7,21 @@
 
 #define STACK_MAXSIZE       100
 
-typedef struct LIFO_Stack_Info
+#define STACK_DATA_TYPE         char
+#define CHAIN_STACK_DATA_TYPE   char
+
+typedef struct Stack_Info
 {
-    int stack_top;
-    int stack_depth;
+    size_t stack_top;
+    size_t stack_depth;
     bool stack_malloc;
 }STACK_INFO_TYPEDEF;
 
-typedef struct LIDO_Stack_Expection
+typedef struct Stack_Expection
 {
     void (*full_stack)(void);
     void (*null_stack)(void);
+    void (*null_heap)(void);
 }STACK_EXPECTION_TYPEDEF;
 
 typedef struct Stack
@@ -27,20 +31,55 @@ typedef struct Stack
     
     bool (*push)(struct Stack* stack, char atom);
     bool (*pop)(struct Stack* stack, char* atom);
-    bool (*multi_pop)(struct Stack* stack, char* array, int depth);
+    bool (*multi_pop)(struct Stack* stack, char* array, size_t depth);
 
     STACK_EXPECTION_TYPEDEF expection;
 }STACK_TYPEDEF,*STACK_TYPEDEF_PTR;
 
+typedef struct Chain_Stack_Info
+{
+    size_t stack_crt_depth;
+    size_t stack_max_depth;
+    bool stack_malloc;
+}CHAIN_STACK_INFO_TYPEDEF;
+
+typedef struct Chain_Stack_Node
+{
+    struct Chain_Stack_Node* next;
+    CHAIN_STACK_DATA_TYPE data;
+}CHAIN_STACK_NODE_TYPEDEF,* CHAIN_STACK_NODE_TYPEDEF_PTR;
+
+typedef struct Chain_Stack_Expection
+{
+    void (*full_stack)(void);
+    void (*null_stack)(void);
+    void (*null_heap)(void);
+}CHAIN_STACK_EXPECTION_TYPEDEF;
+
+typedef struct Chain_Stack
+{
+    CHAIN_STACK_INFO_TYPEDEF __info;
+    CHAIN_STACK_NODE_TYPEDEF_PTR top;                           // 已知头节点
+
+    bool (*push)(struct Chain_Stack* stack, CHAIN_STACK_DATA_TYPE atom);
+    bool (*pop)(struct Chain_Stack* stack, CHAIN_STACK_DATA_TYPE* atom);
+    bool (*multi_pop)(struct Chain_Stack* stack, CHAIN_STACK_DATA_TYPE* array, size_t depth);
+
+    CHAIN_STACK_EXPECTION_TYPEDEF expection;
+}CHAIN_STACK_TYPEDEF, * CHAIN_STACK_TYPEDEF_PTR;
+
 typedef struct Stack_Control
 {
- /*   struct
-    {
-        int* stack_addr[100];
-    }__info;*/
 
-    bool (*init)(STACK_TYPEDEF_PTR stack, int depth);
+    bool (*init)(STACK_TYPEDEF_PTR stack, size_t depth);
     bool (*free)(STACK_TYPEDEF_PTR stack);
+
+    struct
+    {
+        bool (*init)(CHAIN_STACK_TYPEDEF_PTR stack, size_t depth);
+        bool (*free)(CHAIN_STACK_TYPEDEF_PTR stack);
+    }chain_stack;
+
 }STACK_CONTROL_TYPEDEF;
 
 STACK_CONTROL_TYPEDEF stack_ctrl;
