b857fa61a804b2c38d331e278e2c8437c1abd8ab
Miao-Mico <miao.mi.co.io@gmail.com>
2020-04-02 20:43:13 +0800

    增加 struct iterator_unit_s； 暂存，等待更新 allocator；

--- Iterator/Iterator Adaptor Definition/iterator_adaptor_def.h
+++ Iterator/Iterator Adaptor Definition/iterator_adaptor_def.h
@@ -30,6 +30,44 @@
  *********************************************************************************************************
  */
 
+/**
+ * @brief This type is the iterator control structure.
+ */
+
+struct iterator_control_s {
+    struct {
+        errno_t(*init)(struct iterator_s **iterator,
+                       struct iterator_object_unit_s object_unit);
+
+        errno_t(*destroy)(struct iterator_s **iterator);
+    }configuration;
+
+    struct {
+        void *(*advance)(struct iterator_s *iterator,
+                         int step);
+
+        size_t(*distance)(struct iterator_s *iterator);
+    }iterator_operations;
+
+    struct {
+        size_t(*size)(struct iterator_s *iterator);
+
+        bool (*empty)(struct iterator_s *iterator);
+
+        void *(*data)(struct iterator_s *iterator);
+    }range_access;
+};
+
+/**
+ * @brief This type is the unit of the iterator.
+ */
+
+struct iterator_unit_s {
+    struct iterator_control_s *control_ptr;
+
+    void *iterator_ptr;
+};
+
 /*
  *********************************************************************************************************
  *								            FUNCTION PROTOTYPES
--- Iterator/Iterator Adaptor Definition/iterator_adaptor_pte_def.h
+++ Iterator/Iterator Adaptor Definition/iterator_adaptor_pte_def.h
@@ -30,6 +30,27 @@
  *********************************************************************************************************
  */
 
+/**
+ * @brief This type is the iterator adaptor structure.
+ */
+
+struct iterator_adaptor_s {
+    /* @brief RESERVED This variables will record the identity code of iterator_ptr type.				*/
+    enum iterator_type_e iterator_type_id;
+
+    /* @brief This variables will point to the allocator_ptr control.									*/
+    struct allocator_control_s *allocator_control_ptr;
+
+    /* @brief This variables will point to the allocator_ptr.											*/
+    void *allocator_ptr;
+
+	/* @brief This variables is the unit of the iterator.											    */
+    struct iterator_unit_s iterator_unit;
+
+    /* @brief This variables will be a external addon, validity only depends on memory allocation.		*/
+    char addon[0];
+};
+
 /*
  *********************************************************************************************************
  *								            FUNCTION PROTOTYPES
