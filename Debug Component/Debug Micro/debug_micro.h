f553da26238c367c5d3cb1710e4c75f614483a89
Miao-Mico <miao.mi.co.io@gmail.com>
2020-04-28 17:48:08 +0800

    增加 _FUNCTION_LIKE；

--- Debug Component/Debug Micro/debug_micro.h
+++ Debug Component/Debug Micro/debug_micro.h
@@ -22,53 +22,58 @@
  *********************************************************************************************************
  */
 
-/* Define			debug micro control left bracket.								        */
-#define DEBUG_MICRO_CONTROL_LEFTBRACKET                                                     \
+/* Define			debug micro control left bracket.								                    */
+#define DEBUG_MICRO_CONTROL_LEFTBRACKET                                                                 \
     (
 
-/* Define			debug micro control expand.								                */
-#define DEBUG_MICRO_CONTROL_EXPAND(...)                                                     \
+/* Define			debug micro control expand.								                            */
+#define DEBUG_MICRO_CONTROL_EXPAND(...)                                                                 \
     (__VA_ARGS__)
 
-/* Define			debug micro control string.								                */
-#define DEBUG_MICRO_CONTROL_STRING(...)                                                     \
+/* Define			debug micro control string.								                            */
+#define DEBUG_MICRO_CONTROL_STRING(...)                                                                 \
     (#__VA_ARGS__)
 
-/* Define			debug micro control get the arg1 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG1(arg1, ...)                                         \
+/* Define			debug micro control function-like.										            */
+#define DEBUG_MICRO_CONTROL_FUNCTION_LIKE(...)                                                          \
+    do {                                                                                                \
+        __VA_ARGS__;                                                                                    \
+    } while (0)
+
+/* Define			debug micro control get the arg1 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG1(arg1, ...)                                                     \
     (arg1)
 
-/* Define			debug micro control get the arg2 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG2(arg1, arg2, ...)                                   \
+/* Define			debug micro control get the arg2 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG2(arg1, arg2, ...)                                               \
     (arg2)
 
-/* Define			debug micro control get the arg3 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG3(arg1, arg2, arg3, ...)                             \
+/* Define			debug micro control get the arg3 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG3(arg1, arg2, arg3, ...)                                         \
     (arg3)
 
-/* Define			debug micro control get the arg4 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG4(arg1, arg2, arg3, arg4, ...)                       \
+/* Define			debug micro control get the arg4 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG4(arg1, arg2, arg3, arg4, ...)                                   \
     (arg4)
 
-/* Define			debug micro control get the arg5 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG5(arg1, arg2, arg3, arg4, arg5, ...)                 \
+/* Define			debug micro control get the arg5 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG5(arg1, arg2, arg3, arg4, arg5, ...)                             \
     (arg5)
 
-/* Define			debug micro control get the arg6 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG6(arg1, arg2, arg3, arg4, arg5, arg6, ...)           \
+/* Define			debug micro control get the arg6 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG6(arg1, arg2, arg3, arg4, arg5, arg6, ...)                       \
     (arg6)
 
-/* Define			debug micro control get the arg7 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG7(arg1, arg2, arg3, arg4, arg5, arg6, arg7, ...)     \
+/* Define			debug micro control get the arg7 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG7(arg1, arg2, arg3, arg4, arg5, arg6, arg7, ...)                 \
     (arg7)
 
-/* Define			debug micro control get the arg8 of the va_arg.							*/
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG8(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8,    \
-    ...)                                                                                    \
+/* Define			debug micro control get the arg8 of the va_arg.							            */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG8(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, ...)           \                                                                                             \
     (arg8)
 
-/* Define			debug micro control get the cnt-th arg of the va_arg.				    */
-#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG(cnt, ...)                                           \
+/* Define			debug micro control get the cnt-th arg of the va_arg.				                */
+#define DEBUG_MICRO_CONTROL_VA_ARGS_ARG(cnt, ...)                                                       \
     (DEBUG_MICRO_CONTROL_VA_ARGS_ARG ## cnt DEBUG_MICRO_CONTROL_LEFTBRACKET __VA_ARGS__))
 
 /*
